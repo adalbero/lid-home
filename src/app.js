d976b78ea1ac5af7daf27c288c345cbdb2d4a08c
Adalbero <adalbero@gmail.com>
2020-04-07 22:47:46 +0200

    material

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// import { MDCRipple } from '@material/ripple/index';
+// const ripple = new MDCRipple(document.querySelector('.foo-button'));
+// const ripple2 = new MDCRipple(document.querySelector('.ok-button'));
+
+import { MDCRipple } from '@material/ripple/index';
+new MDCRipple(document.querySelector('.foo-button'));
+
+console.log('Hello Workd');
