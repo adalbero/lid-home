ea91408a572a939247d62f53e9acba9053b972b8
Adalbero <adalbero@gmail.com>
2020-04-08 10:31:44 +0200

    remove node dependency

--- src/app.js
+++
@@ -1,8 +0,0 @@
-// import { MDCRipple } from '@material/ripple/index';
-// const ripple = new MDCRipple(document.querySelector('.foo-button'));
-// const ripple2 = new MDCRipple(document.querySelector('.ok-button'));
-
-import { MDCRipple } from '@material/ripple/index';
-new MDCRipple(document.querySelector('.foo-button'));
-
-console.log('Hello Workd');
